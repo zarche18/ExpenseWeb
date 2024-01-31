0f9df15405b9bd648d262b7317e47ad91db6af2d
zarchi <nanzar@outrich.com>
2024-01-31 22:50:37 +0700

    initial commit

---
+++ Expenses/Expenses.DataLayer/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Expenses.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expenses.DataLayer
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+
+        }
+        public DbSet<ExpenseModel> ExpenseModels { get; set; }
+    }
+}
---
+++ Expenses/Expenses.Model/ExpenseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Expenses.Model
+{
+    public class ExpenseModel
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public decimal MoneySpent { get; set; }
+        [Required]
+        public DateTime ExpenseDay { get; set; }
+        [Required]
+        public string Category { get; set; }
+    }
+}
